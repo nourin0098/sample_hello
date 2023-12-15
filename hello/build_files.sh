8a1901ab3d0be3ed2293f8918bf1f8123d343cb4
Nourin <nourinkt0098@gmail.com>
2023-12-16 00:34:03 +0530

    add some file

--- hello/build_files.sh
+++ hello/build_files.sh
@@ -1,4 +1,6 @@
+# build_files.sh
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
+mkdir staticfiles_build  # Add this line to create the directory
 # python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
