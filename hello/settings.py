bdb1caef34615a7804710a514411b85abd4e7ba9
Nourin <nourinkt0098@gmail.com>
2023-12-16 01:09:08 +0530

    update some file

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xpbexu7s=h5w@j8foa9haozyp2(d9h)t!psv@kv#on=29_=)xn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','vercel.app','.now.sh']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
