3049cf137c04270661650df32aa23c2033075def
Nourin <nourinkt0098@gmail.com>
2023-12-16 00:36:11 +0530

    updatefiles

--- hello/build_files.sh
+++ hello/build_files.sh
@@ -1,6 +1,7 @@
 # build_files.sh
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-mkdir staticfiles_build  # Add this line to create the directory
+mkdir staticfiles_build
+ls -l  # Add this line to list the contents of the directory
 # python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
