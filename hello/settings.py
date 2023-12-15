41000b1a957f695cefb0ab1d657e8c5f05c98730
Nourin <nourinkt0098@gmail.com>
2023-12-16 00:20:33 +0530

    add some file

--- hello/settings.py
+++ hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xpbexu7s=h5w@j8foa9haozyp2(d9h)t!psv@kv#on=29_=)xn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
 
